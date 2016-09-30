753132f1c5f173198c92a0ccd2356e45f02eefe1
kserin <kserin@gmail.com>
2016-09-30 09:32:30 +0300

    impl

--- webpack-2/webpack.config.js
+++ webpack-2/webpack.config.js
@@ -1,32 +1,32 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   devtool: 'eval-source-map',
   entry:  __dirname + "/app/main.js",
   output: {
-    path: __dirname + "/public",
+    path: __dirname + "/build",
     filename: "bundle.js"
   },
   module: {
-    loaders: [
-      {
-        test: /\.json$/,
-        loader: "json"
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel'
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css?modules'
-      }
-    ]
+   loaders: [
+     { test: /\.json$/, loader: "json" },
+     { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+     { test: /\.css$/, loader: 'style!css?modules!postcss' }
+   ]
   },
 
   postcss: [
     require('autoprefixer')
   ],
-  
+
+  plugins: [
+    new webpack.BannerPlugin("Copyright Areca Software inc."),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"
+    })
+  ],
+
   devServer: {
     contentBase: "./public",
     colors: true,
